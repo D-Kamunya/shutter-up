61eb10e128dde39632d78a265437837523113870
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 14:15:32 +0300

    Create location model schema

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length =100)
+    country = models.CharField(max_length =100)
+    def __str__(self):
+      return self.city
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def del_location(cls,id):
+        cls.objects.get(id).delete() 
