86ca048da2c928b4eeac091ae4aa84b645ab06fb
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 13:19:51 +0300

    Create search path

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.home_page,name='homepage'),
     path(r'albums/category/<category>', views.albums_by_category,name='albumcategory'),
     path(r'albums/location/<location>', views.albums_by_location,name='albumlocation'),
+    path(r'search/', views.search_results, name='search_results'),
 ]
