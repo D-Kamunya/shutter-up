e168db3ec6c44f86c70ca995739832d4a51995ab
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 20:38:44 +0300

    Create gallery image model schema

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime as dt
+import pyperclip
 # Create your models here.
 
 class Location(models.Model):
@@ -23,7 +24,7 @@ class Location(models.Model):
         '''
         Deletes location instance from db
         '''
-        cls.objects.get(id).delete() 
+        cls.objects.get(pk =id).delete() 
 
 
 class Category(models.Model):
@@ -46,4 +47,92 @@ class Category(models.Model):
         '''
         Deletes category instance from db
         '''
-        cls.objects.get(id).delete()             
+        cls.objects.get(pk =id).delete()    
+
+
+
+
+class Image(models.Model):
+    """
+    Image class to define Image Objects
+    """
+    image_name = models.CharField(max_length =150)
+    image_path = models.ImageField(upload_to = 'uploads/')
+    image_description = models.CharField(max_length =255)
+    location = models.ForeignKey(Location,
+    on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,
+    on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_name 
+
+
+    class Meta:
+        ordering = ['upload_date'] 
+
+    def save_image(self):
+      '''
+      Saves image instance to db
+      '''
+      self.save()
+
+
+    @classmethod
+    def get_all_images(cls):
+      '''
+      Returns all image objects from db
+      '''
+      images=cls.objects.all()
+      return images 
+
+
+    @classmethod
+    def get_image_by_id(cls,id):
+      '''
+      Returns image based on its id
+      '''
+      image=cls.objects.get(id=id)
+      return image
+
+
+
+    @classmethod
+    def delete_image(cls,id):
+      '''
+      Deletes image based on its id
+      '''
+      cls.objects.filter(id=id).delete()
+      
+
+    @classmethod
+    def search_image(cls,category_name):
+      '''
+      Allows us to search for an image using its category.
+      '''
+      images=cls.objects.filter(category__name__icontains=category_name)
+      return images
+
+
+
+    @classmethod
+    def filter_by_location(cls,location_id):
+      """
+      Allows us to filter images by the location.
+      """
+      images=cls.objects.filter(location__id=location_id)
+      return images
+
+    @classmethod
+    def copy_image_url(cls,image_id):
+      '''
+      method that copies image url
+      '''
+      img_url=cls.objects.get(id=image_id).image_path.url
+      pyperclip.copy(img_url)    
+
+
+
+
+ 
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home_page,name='homepage'),
     path(r'albums/category/<category>', views.albums_by_category,name='albumcategory'),
     path(r'albums/location/<location>', views.albums_by_location,name='albumlocation'),
     path(r'search/', views.search_results, name='search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
