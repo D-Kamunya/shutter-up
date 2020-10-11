65005d668649caa5db4e655ea4068853252f02c6
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 13:12:36 +0300

    Add albums_by_category view fn

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from django.http  import HttpResponse
 # Create your views here.
 def home_page(request):
     return render(request,'index.html')
+
+
+def albums_by_category(request,category):
+    return render(request,'search.html')    
