9cc8181ec0515ece7b569793e54d06d58c929630
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 13:11:18 +0300

    Add albumcategory path

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_page,name='homepage'),
+    path(r'albums/category/<category>', views.albums_by_category,name='albumcategory'),
 ]
