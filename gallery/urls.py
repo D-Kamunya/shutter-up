a91caec9f55c01dbdd1e03de8af3f48015b580c5
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-11 13:15:23 +0300

    Add albumslocation path

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page,name='homepage'),
     path(r'albums/category/<category>', views.albums_by_category,name='albumcategory'),
+    path(r'albums/location/<location>', views.albums_by_location,name='albumlocation'),
 ]
