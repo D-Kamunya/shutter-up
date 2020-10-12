987af9821f503c023d77db5637e8f2fc1eaeccf4
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-12 16:17:29 +0300

    Display locations dropdown links in the navbar templates

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home_page,name='home_page'),
     path(r'albums/category/<category>', views.albums_by_category,name='album_category'),
-    path(r'albums/location/<location>', views.albums_by_location,name='album_location'),
+    path(r'albums/location/<location_id>', views.albums_by_location,name='album_location'),
     path(r'search/', views.search_results, name='search_results'),
 ]
 
--- gallery/views.py
+++ gallery/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from .models import Image,Category
+from .models import Image,Category,Location
 # Create your views here.
 
 def get_categories():
     return Category.objects.all()
 
+def get_locations():
+    return Location.objects.all()
+
 def home_page(request):
     images=Image.get_all_images()
-    categories=get_categories()
-    return render(request,'index.html',{"images":images,"categories":categories})
+
+
+    return render(request,'index.html',{"images":images,"categories":get_categories(),"locations":get_locations()})
 
 
 def albums_by_category(request,category):
@@ -17,7 +21,7 @@ def albums_by_category(request,category):
 
 
 
-def albums_by_location(request,location):
+def albums_by_location(request,location_id):
     return render(request,'index.html')     
 
 
